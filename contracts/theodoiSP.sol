81acf78a8fa1387b5538c8143ef7678e6a2baf4e
Lê Hoàn Chân <Le.Hoan.Chan@gmail.com>
2024-08-05 08:31:48 +0000

    Viết lại Smart Contract cho đơn giản

--- contracts/theodoiSP.sol
+++ contracts/theodoiSP.sol
@@ -2,8 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract TheoDoiSanPham {
-    address public chuSoHuu;
-    address[] public danhSachNguoiUyQuyen;
+    string[] public danhSachMaSanPham;
     mapping(string => bool) public maSoSanPhamDaTonTai;
     
     struct TrangThai {
@@ -14,7 +13,7 @@ contract TheoDoiSanPham {
         string moTaChiTiet;
     }
     
-    mapping(string => TrangThai[]) public lichSuTrangThaiSanPham;  // Sử dụng mapping
+    mapping(string => TrangThai[]) public lichSuTrangThaiSanPham;
     
     struct SanPham {
         string id;
@@ -29,36 +28,18 @@ contract TheoDoiSanPham {
     mapping(string => SanPham) public danhSachSanPham;
     
     event SanPhamDaTao(string idSanPham, string tenSanPham);
-    
     event CapNhatTrangThai(string idSanPham, string tenTrangThai, string thoiGian, string diaDiem, string loaiTrangThai, string moTaChiTiet);
     
-    event UyQuyen(address nguoiDuocUyQuyen);
-    
-    event ThuHoiUyQuyen(address nguoiBiThuHoi);
-    
-    constructor() {
-        chuSoHuu = msg.sender;
-    }
-    
-    modifier chiChuSoHuu() {
-        require(msg.sender == chuSoHuu, "Chi chu so huu hop dong moi duoc phep goi ham nay");
-        _;
-    }
-    
-    modifier chiUyQuyen() {
-        require(laNguoiUyQuyen(msg.sender), "Chi nguoi uy quyen moi duoc phep goi ham nay");
-        _;
-    }
-    
-    function taoSanPham(string memory id, string memory ten, string memory nhaSanXuat, string memory loaiSanPham, string memory kichThuoc, string memory trongLuong, string memory hanSuDung) public chiChuSoHuu {
+    function taoSanPham(string memory id, string memory ten, string memory nhaSanXuat, string memory loaiSanPham, string memory kichThuoc, string memory trongLuong, string memory hanSuDung) public {
         require(!maSoSanPhamDaTonTai[id], "Ma so da ton tai");
         maSoSanPhamDaTonTai[id] = true;
+        danhSachMaSanPham.push(id);
         
         danhSachSanPham[id] = SanPham(id, ten, nhaSanXuat, loaiSanPham, kichThuoc, trongLuong, hanSuDung);
         emit SanPhamDaTao(id, ten);
     }
 
-    function capNhatTrangThai(string memory idSanPham, string memory tenTrangThai, string memory thoiGian, string memory diaDiem, string memory loaiTrangThai, string memory moTaChiTiet) public chiUyQuyen {
+    function capNhatTrangThai(string memory idSanPham, string memory tenTrangThai, string memory thoiGian, string memory diaDiem, string memory loaiTrangThai, string memory moTaChiTiet) public {
         require(maSoSanPhamDaTonTai[idSanPham], "Ma so khong ton tai");
         
         TrangThai memory trangThai = TrangThai(tenTrangThai, thoiGian, diaDiem, loaiTrangThai, moTaChiTiet);
@@ -70,31 +51,8 @@ contract TheoDoiSanPham {
     function xemLichSuTrangThaiSanPham(string memory idSanPham) public view returns (TrangThai[] memory) {
         return lichSuTrangThaiSanPham[idSanPham];
     }
-    
-    function uyQuyen(address _nguoiUyQuyen) public chiChuSoHuu {
-        danhSachNguoiUyQuyen.push(_nguoiUyQuyen);
-        emit UyQuyen(_nguoiUyQuyen);
-    }
-    
-    function thuHoiUyQuyen(address _nguoiBiThuHoi) public chiChuSoHuu {
-        for (uint i = 0; i < danhSachNguoiUyQuyen.length; i++) {
-            if (danhSachNguoiUyQuyen[i] == _nguoiBiThuHoi) {
-                delete danhSachNguoiUyQuyen[i];
-                emit ThuHoiUyQuyen(_nguoiBiThuHoi);
-            }
-        }
-    }
-    
-    function XemDanhSachNguoiUyQuyen() public view returns (address[] memory) {
-        return danhSachNguoiUyQuyen;
-    }
-    
-    function laNguoiUyQuyen(address _address) public view returns (bool) {
-        for (uint i = 0; i < danhSachNguoiUyQuyen.length; i++) {
-            if (danhSachNguoiUyQuyen[i] == _address) {
-                return true;
-            }
-        }
-        return false;
+
+    function layDanhSachSanPham() public view returns (string[] memory) {
+        return danhSachMaSanPham;
     }
 }
