dcd8fac293fc8c7e9745e1941e5726f652789506
Lê Hoàn Chân <Le.Hoan.Chan@gmail.com>
2024-08-23 02:25:05 +0000

    Thêm tạm phân Quyền

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ app.get("/show", (req, res) => {
     res.sendFile(path.join(__dirname, "/src/show.html"));
 })
 
+app.get("/phanQuyen", (req, res) => {
+    res.sendFile(path.join(__dirname, "/src/phanQuyen.html"));
+})
+
 const server = app.listen(5000);
 const portNumber = server.address().port;
 console.log(`port is open on ${portNumber}`);
